b6930e5b20fd288459648af19d118cd80358ba93
Amjed Ali <amjedmgm@gmail.com>
2021-08-18 23:56:50 +0530

    Updated Node Details

--- src/services/backendcalls.js
+++ src/services/backendcalls.js
@@ -2,13 +2,14 @@ import http from "./httpservice";
 import { config } from "../config"
 
 const apiEndpoint = config.apiEndpoint;
+const node = 'NodeMCU-1.0';
 
 export async function getUserHistory() {
     return await http.get(apiEndpoint + "/me/history")
 }
 
 export async function getSensorLastData() {
-    return await http.get(apiEndpoint + '/sensor/node/ss/last/all')
+    return await http.get(apiEndpoint + '/sensor/node/'+ node +'/last/all')
 }
 
 export async function getSensorList() {
